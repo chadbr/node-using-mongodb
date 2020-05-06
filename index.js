ba4ab3b8668219cfe0b018b5531600888cc11b60
Chad Brockman <chadbr@slb.com>
2020-05-06 11:25:54 -0500

    bah...

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = 4000;
 // create connection
 
 mongoose.Promise = global.Promise;
-mongoose.connect(`mongodb://localhost/productsdb`, {
+mongoose.connect(`mongodb://localhost:27017/productsdb`, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
